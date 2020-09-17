7917bf5585f6c65be9dfc28e78b1efee674b9d35
isenTang <3104255643@qq.com>
2020-09-17 09:47:00 +0800

    js基础

---
+++ day08/static/index.js
@@ -0,0 +1,34 @@
+
+// 声明变量
+var a = 1;
+
+console.log('a ===>',a);
+
+// string
+var str = 'hello world';
+
+console.log(str.length);
+console.log(str.charAt(1))
+console.log(str.replace("world", "mars"));
+
+// number
+var num = 1;
+console.log(Math.max(2,3,4,99));
+console.log(Math.random());
+
+var numStr = '1aaa';
+
+// NaN not a number
+console.log(parseInt(numStr));
+var height = '10px'
+console.log(parseInt(height))
+
+// Boolean
+console.log(Boolean(0))
+
+console.log(Boolean(''))
+
+// null undefined
+var obj1;
+console.log(obj1);
+
